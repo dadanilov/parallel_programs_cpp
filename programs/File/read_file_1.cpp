c4b25367c94a906a884f796dc9ec217e48661e6e
dadanilov <Danilov.Danil@outlook.com>
2021-12-08 02:45:31 +0700

    Исправлена программа

--- programs/File/read_file_1.cpp
+++ programs/File/read_file_1.cpp
@@ -6,20 +6,28 @@
 
 #pragma comment(lib, "pthreadVCE2.lib")
 
-#define THREADS 6
+#define THREADS 4
 
 using namespace std;
 fstream F;
-string* line;
-pthread_mutex_t m;
+
+pthread_mutex_t m, m1;
 pthread_mutexattr_t attr;
 
 void* pthread(void* arg)
 {
 	pthread_mutex_lock(&m);
 	int tpart = *(int*)arg, check;
-	F >> line[tpart];
-	pthread_mutex_unlock(&m);
+	while (!F.eof())
+	{
+		string l;
+		F >> l;
+		pthread_mutex_unlock(&m);
+		pthread_mutex_lock(&m1);
+		cout << "Номер потока = " << tpart << " - " << l << endl;
+		pthread_mutex_unlock(&m1);
+		pthread_mutex_lock(&m);
+	}
 	return NULL;
 }
 
@@ -28,26 +36,19 @@ int _tmain(int argc, _TCHAR* argv[])
 	setlocale(LC_ALL, "Russian");
 	pthread_t t[THREADS];
 	int n[THREADS];
-	line = new std::string[6];
 	pthread_mutexattr_init(&attr);
 	pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE);
 	pthread_mutex_init(&m, &attr);
+	pthread_mutex_init(&m1, &attr);
 	pthread_mutexattr_destroy(&attr);
 	F.open("text.txt");
-	while (!F.eof())
+	for (int i = 0; i < THREADS; i++)
 	{
-		for (int i = 0; i < THREADS; i++)
-		{
-			n[i] = i;
-			
-			pthread_create(&t[i], NULL, pthread, &n[i]);
-		}
+		n[i] = i;	
+		pthread_create(&t[i], NULL, pthread, &n[i]);
 	}
 	for (int i = 0; i < THREADS; i++)
 		pthread_join(t[i], NULL);
-	for (int i = 0; i < 6; i++)
-		cout << line[i];
 	F.close();
-	delete[] line;
 	return 0;
 }
